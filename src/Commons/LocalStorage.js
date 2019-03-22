c91f78e87510d9a0ce91ad8f4c680b40bab69282
Briam <briammartinez@gmail.com>
2019-03-22 02:56:25 +0100

    Implemetation of localStorage methods

---
+++ src/Commons/LocalStorage.js
@@ -0,0 +1,51 @@
+import uniqBy from 'lodash/uniqBy';
+
+const api = () => {
+  if (typeof(Storage) !== "undefined") {
+    const get = (key) => {
+      const value =  localStorage.getItem(key);
+      return JSON.parse(value) || [];
+    };
+    const set = (key, value) => {
+      const stringValue = JSON.stringify(value);
+      localStorage.setItem(key, stringValue);
+    }
+    return {
+      get,
+      set,
+      update: (key, id, movie) => {
+        const list = get(key);
+        const listUpdated = list.map((obj) => {
+          if (obj.id === id) {
+            const oldMovies = obj.movies || [];
+            return { ...obj, movies: uniqBy([...oldMovies, movie], 'id') };
+          }
+          return { ...obj };
+        });
+        set(key, listUpdated);
+      },
+      getCollection: (key, id) => {
+        const list = get(key);
+        return list.find((obj) => obj.id === id);
+      },
+      deleteMovie: (key, idCollection, idMovie) => {
+        const list = get(key);
+        const listUpdated = list.map((obj) => {
+          if (obj.id === Number(idCollection)) {
+            const filterMovies = obj.movies ? obj.movies.filter((movie) => movie.id !== Number(idMovie)) : [];
+            return { ...obj, movies: [...filterMovies]  };
+          }
+          return { ...obj };
+        });
+        set(key, listUpdated);
+      }
+    };
+  } else {
+    return {
+      get: () => console.warn('Localstorage not supported'),
+      set: () => console.warn('Localstorage not supported'),
+    };
+  }
+};
+
+export default api;
