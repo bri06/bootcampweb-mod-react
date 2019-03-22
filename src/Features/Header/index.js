9a6f7090ef25fdc5d4e4d10ba7ed9684de78b9c5
Briam <briammartinez@gmail.com>
2019-03-22 02:44:20 +0100

    Created header component

---
+++ src/Features/Header/index.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import logo from '../../Assets/video-camera.png';
+import './header.css'
+
+class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      inputSearch: ''
+    };
+  }
+
+  performSearch = (e) => {
+    this.setState({ inputSearch: e.target.value })
+    this.props.searchChangeHandler(e.target.value);
+  };
+
+  render() {
+    return(
+      <div className="header">
+        <div className="header-links">
+          <div className="img-container">
+            <a href="/home"><img className="logo" src={logo} alt="logo"/></a>
+          </div>
+          <a href="/home">Home</a>
+          <a href="/mycollections">My collections</a>
+        </div>
+        <div className="movie-search">
+          <form onSubmit={(e) => e.preventDefault()}>
+            <input type="text" className="input-search" onChange={this.performSearch} placeholder="Type to search a movie..." />
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+Header.defaultProps = {
+  searchChangeHandler: () => '',
+};
+
+export default Header;
